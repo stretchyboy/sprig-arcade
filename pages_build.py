14e6ea9603e73180b985a2cc692310b4e43d2ede
Mr Eggleton <meggleton@utcsheffield.org.uk>
2025-11-14 14:00:10 +0000

    TODOs for pages_build.py

---
+++ pages_build.py
@@ -0,0 +1,14 @@
+# builds the pages after the first part of build.py has run.
+# The TODO items are straight out of the diagram
+
+# TODO : data = read metadata.json
+
+# TODO : load game page template
+
+# TODO : Foreach game in data
+    # TODO : filepath ='/build/games/{game}/index.html'
+    # TODO : Render game page to filepath
+    
+# TODO : load gallery page template
+# TODO : Render gallery page to index.html file in /build 
+    
